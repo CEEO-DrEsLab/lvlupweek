d28106d6cde0ec671f8e4bee8008bef93bb080e2
Iguanotron <clucasfoley@gmail.com>
2016-06-10 16:15:46 -0400

    checks for empty query string

--- handler1.php
+++ handler1.php
@@ -64,7 +64,7 @@ if($_GET['exhibit'])
 // This signals the page to load from Exhibits/HOMEPAGE
 function getExhibitName() {
 	var matches = RegExp('[?&]exhibit=([^&]*)').exec(window.location.search);
-	return matches == null || matches.length <= 1 ? "HOMEPAGE" : decodeURIComponent(matches[1].replace(/\+/g, ' ')); // replace + with space
+	return matches == null || matches.length <= 1 || matches[1] == "" ? "HOMEPAGE" : decodeURIComponent(matches[1].replace(/\+/g, ' ')); // replace + with space
 }
 // Sets the title of the window and the title of the description to the contents of title.txt
 function setTitle(exhibitName) {
