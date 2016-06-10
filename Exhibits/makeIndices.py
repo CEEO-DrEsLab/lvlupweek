62b6acd9ae73b4bc397c0cb9421c13d40441156b
Iguanotron <clucasfoley@gmail.com>
2016-06-10 11:34:47 -0400

    Optimized makeIndices.py

--- Exhibits/makeIndices.py
+++ Exhibits/makeIndices.py
@@ -1,16 +1,17 @@
 # makeIndices.py
 # Last edited by Caleb Lucas-Foley on 2016-07-10
+#
+# Makes index.json files for videos and images
 import os
 import json
 
 for project in next(os.walk('.'))[1]:
-	if not project == 'HOMEPAGE':
-		for folder in ['images', 'videos']:
-			mediaDirectory = './' + project + '/' + folder
-			if not os.path.exists(mediaDirectory):
-				os.makedirs(mediaDirectory)
-			index = open(mediaDirectory + '/index.json', 'w')
-			media = os.listdir(mediaDirectory)
-			media = [item for item in media if not item.endswith('.json')]
-			json.dump(media, index)
-			index.close()
+	for folder in ['images', 'videos']:
+		mediaDirectory = './' + project + '/' + folder
+		if not os.path.exists(mediaDirectory):
+			os.makedirs(mediaDirectory)
+		index = open(mediaDirectory + '/index.json', 'w')
+		media = os.listdir(mediaDirectory)
+		media = [item for item in media if not item.endswith('.json')]
+		json.dump(media, index)
+		index.close()
