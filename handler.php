b347d485a2ae1d523e20f942e30a7dcc59daf691
Iguanotron <clucasfoley@gmail.com>
2016-06-15 15:00:56 +0000

    added id and redir values to handler.php, but I wanted to make it funnt

--- handler.php
+++ handler.php
@@ -1,8 +1,10 @@
 <html>
 <head>
 <?php
-//echo "hello there!";
-if($_GET['info'])
+
+echo "hi there!<br \>\n";
+
+/*if($_GET['info'])
 {
 	$info = strip_tags($_GET['info']);
 
@@ -13,29 +15,49 @@ if($_GET['info'])
 	);
 	$exhibit = $codes_array[$info];
 	echo "OK-1<br \>\n";
-//	$result = shell_exec($command);
 	$command = file_put_contents("exhibits.txt", $exhibit . "\n", FILE_APPEND);
-	echo "OK-2<br \>\n";
-	echo $exhibit . "<br \>\n";
-	echo "OK-3<br \>\n";
-	echo "bytes: " . $command . "<br \>\n";
-	echo "OK-4<br \>\n";
+//	echo "OK-2<br \>\n";
+//	echo $exhibit . "<br \>\n";
+//	echo "OK-3<br \>\n";
+//	echo "bytes: " . $command . "<br \>\n";
+//	echo "OK-4<br \>\n";
 
 	$newURL = 'http:/' . '/130.64.95.38/slideshow.php?exhibit=' . $exhibit;
 
-	/*$command = 'echo "raspberry" | (sudo -u pi python RPWifi.py ' . escapeshellcmd($exhibit) . ')';
-	$command = 'python RPWifi.py ' . escapeshellcmd($exhibit);
-	if(substr($command,strlen($command)-1,1) == "/")
-	{
-		$command = substr($command,0,strlen($command)-1);
-	}
-	echo "<br \>\nRUN COMMAND: $command<br \>\n";
-	$output = shell_exec($command);
-	echo str_replace("\n","<br \>\n",$output);
-	echo "<br \>\n";*/
+}*/
+
+if($_GET['id'])
+{
+        $id = strip_tags($_GET['id']);
+
+        $codes_array = array(
+                "a3" => "GuessTheAnimal",
+                "a2" => "BongoBonanza",
+                "a1" => "NutcrackerPuppet"
+        );
+        $exhibit = $codes_array[$id];
+        echo "OK-2<br \>\n";
+        $command = file_put_contents("exhibits.txt", $exhibit . "\n", FILE_APPEND);
+//      echo "OK-2<br \>\n";
+//      echo $exhibit . "<br \>\n";
+//      echo "OK-3<br \>\n";
+//      echo "bytes: " . $command . "<br \>\n";
+//      echo "OK-4<br \>\n";
+
+        $newURL = 'http:/' . '/130.64.95.38/slideshow.php?exhibit=' . $exhibit;
+
 }
-//?>
-<script language=JavaScript>window.location.href="<?php echo $newURL ?>"</script>
+
+if($_GET['redir'])
+{
+	echo "redir was here!";
+}
+
+/*if($_GET['redir'])
+{
+	//If signal comes from non-Arduino: pull up webpage on source device
+	<script language=JavaScript>window.location.href="<?php echo $newURL ?>"</script>
+}*/
 
 ?>
 </head></html>
